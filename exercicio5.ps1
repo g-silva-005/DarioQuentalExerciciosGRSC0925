909b501bc7a875bdfad826ee173321a7601a6053
Gustavo <1999goncalo.gs@gmail.com>
2026-02-23 10:24:48 +0000

    exercicio 5

---
+++ exercicio5.ps1
@@ -0,0 +1,14 @@
+﻿$x = read-Host "Introduza uma mensagem: "
+
+switch ($x) {
+    {$_ -in "oi", "ola", "bom dia"} {
+        write-host "Saudacao"
+    }
+
+    {$_.EndsWith("?")} {
+        Write-Host "Pergunta"
+    }
+
+    {$_.contains("adeus") -or $_.contains("tchau")} {´        Write-Host "despedida"
+    }
+}
