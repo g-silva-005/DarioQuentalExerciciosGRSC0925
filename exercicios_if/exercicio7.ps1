394e863e62f5035de5dee3da72d29ea2e736f33d
Gustavo <1999goncalo.gs@gmail.com>
2026-02-23 14:56:07 +0000

    exercicio 7

---
+++ exercicios_if/exercicio7.ps1
@@ -0,0 +1,12 @@
+﻿[float]$nota1 = Read-Host "Insira a primeira nota "
+[float]$nota2 = Read-Host "Insira a segunda nota "
+[float]$nota3 = Read-Host "Insira a terceira nota "
+[int]$media = ($nota1 + $nota2 + $nota3)/3
+
+if ($media -ge 6) {
+    Write-Host "Média: $media | Aprovado"
+ }
+
+ else {
+    Write-Host "Média : $media | Reprovado"
+ }
