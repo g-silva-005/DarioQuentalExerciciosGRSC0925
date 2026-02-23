14cf8df362fb3c22aa7f57ebb35244249d2f3b54
Gustavo <1999goncalo.gs@gmail.com>
2026-02-23 10:33:06 +0000

    exercicio 6

---
+++ exercicio6.ps1
@@ -0,0 +1,25 @@
+﻿$status = Read-Host "Introduza o status do seu servidor: "
+[int]$tempo_resposta = Read-Host "Introduza o tempo de resposta: "
+
+$dicionario = @{
+    "status" = $status
+    "tempo_resposta" = $tempo_resposta
+}
+
+switch ($dicionario.status){
+    {$_ -in "ok" -and $dicionario.tempo_resposta -le 200} {
+        write-host "Servidor ativo"
+    }
+
+    {$_ -in "ok" -and  $dicionario.tempo_resposta -gt 200} {
+        write-host "Servidor lento"
+    }
+
+    {$_ -in "erro"} {
+        write-host "Servidor indisponivel"
+    }
+
+    Default {
+        Write-Host "Entrada inválida. Por Favor insira um dado válido" 
+    }
+}
