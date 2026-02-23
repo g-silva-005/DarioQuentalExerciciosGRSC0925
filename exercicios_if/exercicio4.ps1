fec02c4437f1e094d936001288a87977aadee092
Gustavo <1999goncalo.gs@gmail.com>
2026-02-23 14:01:07 +0000

    exercicio 4

---
+++ exercicios_if/exercicio4.ps1
@@ -0,0 +1,17 @@
+﻿$saldo = Read-Host "Introduza o seu saldo "
+$cheque = Read-Host "Introduza o valor que quer fazer o cheque "
+$novosaldo = 0
+
+if ($saldo -ge $cheque) {
+    $novosaldo = $saldo - $cheque
+    Write-Host "Cheque descontado | Novo saldo: $novosaldo "
+}
+
+elseif ($saldo -lt $cheque) {
+    Write-Host "O saldo da sua conta é inferior ao valor que pretende descontar para fazer o cheque!"
+}
+
+else {
+    Write-Host "Dados inválidos"
+}
+
