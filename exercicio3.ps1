803fc7dfb23776812dede05de9fb72082a5cec9d
Gustavo <1999goncalo.gs@gmail.com>
2026-02-23 08:58:28 +0000

    exercicio 3

---
+++ exercicio3.ps1
@@ -0,0 +1,21 @@
+﻿$tipo = Read-Host "Introduza o tipo (compra ou venda): "
+[float]$valor = Read-Host "Introduza o valor: "
+
+$dicionario = @{
+    "tipo" = $tipo
+    "valor" = $valor
+}
+
+switch ($dicionario.tipo){
+    {$_ -in "compra"} {
+        write-host "Compra de $($dicionario.valor)€"
+    }
+
+    {$_ -in "venda"} {
+        write-host "Compra de $($dicionario.valor)€"
+    }
+
+    Default {
+        Write-Host "Entrada inválida. Por Favor insira um dado válido" 
+    }
+}
