f213a59249192d596897fe4311b4dbb8d489d457
Gustavo <1999goncalo.gs@gmail.com>
2026-02-23 13:54:17 +0000

    exercicio 3

---
+++ exercicios_if/exercicio3.ps1
@@ -0,0 +1,10 @@
+﻿$num1 = Read-Host "Insira o primeiro numero "
+$num2 = Read-Host "Insira o segundo numero "
+
+if ($num1 -gt $num2) {
+    Write-Host "Crescente: $num2, $num1 | Decrescente: $num1, $num2"
+}
+
+else {
+    Write-Host "Crescente: $num1, $num2 | Decrescente: $num2, $num1"
+}
