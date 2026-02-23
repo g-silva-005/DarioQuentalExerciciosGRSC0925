a5df5f47f4f020b8da7a409fc4ee7d6eaf17f571
Gustavo <1999goncalo.gs@gmail.com>
2026-02-23 12:27:36 +0000

    exercicio1

---
+++ exercicios_if/exercicio1.ps1
@@ -0,0 +1,7 @@
+﻿$tempo = Read-Host "Insira o tempo que deseja calcular (em segundos) "
+[int]$horas = $tempo/3600
+$resto = $tempo%3600
+[int]$minutos = $resto/60
+$segundos = $resto%60
+
+Write-Host "Horas: $horas | Minutos: $minutos | Segundos: $segundos"
