3b9c3cfad6d465653f5ebd6564ebfddc295565de
Gustavo <1999goncalo.gs@gmail.com>
2026-02-23 08:34:47 +0000

    exercicio 1

---
+++ exercicio1.ps1
@@ -0,0 +1,15 @@
+﻿$dia = Read-Host "Introduza o dia da semana"
+
+switch ($dia) {
+    {$_ -in "segunda", "terca", "quarta", "quinta", "sexta"} {
+        write-host "Dia útil"
+    }
+
+    {$_ -in "sabado", "domingo"} {
+        write-host "Fim de Semana"
+    }
+
+    Default {
+        Write-Host "Entrada inválida. Por Favor insira um dado válido" 
+    }
+}
