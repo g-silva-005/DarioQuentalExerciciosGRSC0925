fcc00198c1f527f754456a14ed00a41c5c205138
Gustavo <1999goncalo.gs@gmail.com>
2026-02-23 10:08:29 +0000

    exercicio 4

---
+++ exericicio4.ps1
@@ -0,0 +1,24 @@
+﻿
+$x = Read-Host "Introduza um valor"
+
+switch ($x) {
+    { $_.Contains(",") } {
+        Write-Host "Lista"
+        break
+    }
+
+    { $_.Contains(".") } {
+        Write-Host "Número decimal"
+        break
+    }
+
+    { $null -ne ($_ -as [int]) } {
+        Write-Host "Número inteiro"
+        break
+    }
+
+    Default {
+        Write-Host "String textual"
+        break
+    }
+}
