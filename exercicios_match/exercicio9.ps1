177cdabf4c1abd5053dec34f83d2f0a8781343db
Gustavo <1999goncalo.gs@gmail.com>
2026-02-23 11:45:56 +0000

    exercicio 9

---
+++ exercicios_match/exercicio9.ps1
@@ -0,0 +1,28 @@
+﻿$metodo=Read-Host "Introduza o metodo (escreva em caps lock) "
+$conteudo=Read-Host "Introduza o conteudo (escreva em caps lock) "
+
+$dicionario = @{
+    "metodo"=$metodo
+    "conteudo"=$conteudo
+}
+
+switch ($dicionario.metodo) {
+    {$dicionario.metodo -in "GET"} {
+        Write-Host "Requisição GET recebida"
+        break
+    }
+
+    {$dicionario.metodo -in "POST" -and $dicionario.conteudo -ne ""} {
+        Write-Host "Requisição POST com dados válidos"
+        break
+    }
+
+    {$dicionario.metodo -in "POST" -and $dicionario.conteudo -eq ""} {
+        Write-Host "Requisição POST sem dados"
+        break
+    }
+
+    Default {
+        Write-Host "Entrada inválida. Por Favor insira um dado válido" 
+    }
+}
