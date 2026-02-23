538c65aff9615a0555dddc00f8fbd491d17f7b77
Gustavo <1999goncalo.gs@gmail.com>
2026-02-23 11:19:52 +0000

    exercicio 8

---
+++ exercicio8.ps1
@@ -0,0 +1,33 @@
+﻿$operacao = Read-Host "Introduza a operação que pretende fazer "
+[float]$numero1 = read-host "Introduza o primeiro numeiro "
+[float]$numero2 = read-host "Introduza o segundo numeiro "
+$soma=0
+$subtrai=0
+$multiplica=0
+$divide=0
+
+switch($operacao) {
+    {$operacao -in "soma"} {
+        $soma=$numero1 + $numero2
+        write-host "A soma é $soma" 
+    }
+
+    {$operacao -in "subtrai"} {
+        $subtrai=$numero1 - $numero2
+        write-host "A subtracacao é $subtrai" 
+    }
+
+    {$operacao -in "multiplica"} {
+        $multiplica=$numero1 * $numero2
+        write-host "A multiplicacao é $multiplica" 
+    }
+
+    {$operacao -in "divide"} {
+        $divide=$numero1 / $numero2
+        write-host "A divisao é $divide" 
+    }
+
+    Default {
+        Write-Host "Entrada inválida. Por Favor insira um dado válido" 
+    }
+}
