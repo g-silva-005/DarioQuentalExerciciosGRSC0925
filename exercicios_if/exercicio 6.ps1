ccfaec96075ab1f9c82388646d1e39886fc4711c
Gustavo <1999goncalo.gs@gmail.com>
2026-02-23 14:45:11 +0000

    exercicio 6

---
+++ exercicios_if/exercicio 6.ps1
@@ -0,0 +1,24 @@
+﻿[string]$nome = Read-Host "Introduza o seu nome "
+[float]$compra = Read-Host "Introduza o valor da sua compra "
+[float]$desconto = 0.0
+[float]$total = 0.0
+
+
+if ($compra -le 200) {
+    $desconto =  $compra * 0.10
+    $total = $compra - $desconto
+    Write-Host "Nome: $nome | Compra: $compra | Desconto: $desconto | Total a pagar: $total"
+}
+
+elseif ($compra -gt 200 -and $compra -le 500) {
+    $desconto =  $compra * 0.15
+    $total = $compra - $desconto
+    Write-Host "Nome: $nome | Compra: $compra | Desconto: $desconto | Total a pagar: $total"
+}
+
+else {
+    $desconto =  $compra * 0.20
+    $total = $compra - $desconto
+    Write-Host "Nome: $nome | Compra: $compra | Desconto: $desconto | Total a pagar: $total"
+}
+
