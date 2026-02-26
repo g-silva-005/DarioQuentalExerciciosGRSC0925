b44d66cf872276872176d87fddc329fe094344cc
Gustavo <1999goncalo.gs@gmail.com>
2026-02-26 11:00:01 +0000

    exercicio 8

---
+++ exercicios_if/exercicios 8.ps1
@@ -0,0 +1,31 @@
+﻿$i = 0
+$x = 0
+[int]$media = 0
+[int]$soma = 0
+$nota = @()
+
+while ($i -lt 10) {
+    [int]$n = Read-Host "Introduza uma nota "
+    $nota += $n
+    $soma += $n
+    $i++
+}
+
+$media = $soma / 10
+
+$notasIguais = 0
+
+while ($x -lt 10) {
+    if ($nota[$x] -ge $media) {
+        $notasIguais++
+    }
+    $x++
+}
+
+Write-Host "A media das notas e de $media | Existem $notasIguais alunos com uma nota maior ou igual a media das notas"
+
+
+
+
+
+
