5eaf182a45ec8afd9603021ae3d3f1a9ae451d78
Gustavo <1999goncalo.gs@gmail.com>
2026-03-02 14:54:51 +0000

    exercicio 2

--- exercicios_loops/exercicio1.ps1
+++ exercicios_loops/exercicio1.ps1
@@ -2,10 +2,11 @@
 
 while ($i -le 60){
     if ($i % 2 -eq 0){
-          Write-Host("$i | Numero par")   
+        Write-Host("$i | Numero par")   
     }
     else {
         Write-Host("$i | Numero impar")
     }
     $i++
 }
+
---
+++ exercicios_loops/exercicio2.ps1
@@ -0,0 +1,13 @@
+﻿$i=1
+
+while ($i -le 10){
+    $x = Read-Host("escreva o $i º numero: ")
+    
+    if ($x % 2 -eq 0){
+          Write-Host("$x | Numero par")   
+    }
+    else {
+        Write-Host("$x | Numero impar")
+    }
+    $i++
+}
