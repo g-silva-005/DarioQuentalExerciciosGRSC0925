8fd4a79e44e08713d6e626838e7b29f861d34875
Gustavo <1999goncalo.gs@gmail.com>
2026-03-02 15:05:54 +0000

    exercicio 3

---
+++ exercicios_loops/exercicio3.ps1
@@ -0,0 +1,14 @@
+﻿[int]$media=0
+[int]$soma=0
+
+
+for ($i=1;$i -le 10;$i++){
+    $x = Read-Host("Introduza a $i º nota: ")
+
+    $soma+=$x
+
+    $media= $soma / 10
+}
+
+Write-Host("")
+Write-Host("A media das notas e de $media")
