acd91007daeb6fcfa4fc0cdbfcb4788a04db1eab
Gustavo <1999goncalo.gs@gmail.com>
2026-03-02 15:34:23 +0000

    exercicio 4

---
+++ exercicios_loops/exercicio4.ps1
@@ -0,0 +1,22 @@
+﻿
+
+for ($i=1;$i -le 5;$i++){
+    $x = Read-Host("Introduza o $i º numero: ")
+    
+    $divisores=0
+    $g=1
+
+    while ($g -le $x){
+        if ($x % $g -eq 0){
+        $divisores++
+        }
+        $g++
+    }
+    
+    if ($divisores -eq 2){
+        Write-Host("Parabens!! O numero $x e primo")
+    }
+    else {
+        Write-Host("O numero $x nao e primo")
+    }
+}
