73b60aedf2ddc9ff0df30402bb9c132918ee3c60
Gustavo <1999goncalo.gs@gmail.com>
2026-03-09 13:48:47 +0000

    exercicio 6

---
+++ exercicios_loops/exercicio6.ps1
@@ -0,0 +1,24 @@
+﻿$numeroXD = 2
+$primaco = 0
+
+while ($primaco -le 9){
+    $divisores = 0
+    $g = 1
+        
+        while ($g -le $numeroXD){
+            if ($numeroXD % $g -eq 0){
+                $divisores++
+            }
+            $g++
+        }
+
+    if ($divisores -eq 2){
+        Write-Host "$numeroXD"
+        $primaco++
+    }
+
+   $numeroXD++
+
+}
+
+
