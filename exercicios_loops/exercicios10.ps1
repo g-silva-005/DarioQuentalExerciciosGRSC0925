6fb343a194f5e6a63e5c79d795c92b8f8b73bb0d
Gustavo <1999goncalo.gs@gmail.com>
2026-03-09 14:49:49 +0000

    exercicio 10

---
+++ exercicios_loops/exercicios10.ps1
@@ -0,0 +1,18 @@
+﻿$i = 1
+
+$num = Read-Host "Introduza um numero que pretende saber os seus divisores: "
+
+$contardivisor = 0
+
+while ($i -le $num){
+    
+    if ($num % $i -eq 0){
+        $contardivisor++    
+    }
+    
+    $i++
+}
+
+Write-Host ""
+Write-Host "O numero $num tem $contardivisor divisores no total"
+
