f82a4894f2eefced517e8af540e7d892e1615e8f
Gustavo <1999goncalo.gs@gmail.com>
2026-03-09 14:01:42 +0000

    exercicio 8

---
+++ exercicios_loops/exercicio8.ps1
@@ -0,0 +1,16 @@
+﻿$i = 10
+$g = 15
+
+Read-Host "Prima ENTER para comecar a sequencia 1..."
+
+while ($i -le 1000){
+    Write-Host "numero: $i"
+    $i+=10
+}
+
+Read-Host "Prima ENTER para comecar a sequencia 2..."
+
+while ($g -le 995){
+    Write-Host "numero: $g"
+    $g+=10
+}
