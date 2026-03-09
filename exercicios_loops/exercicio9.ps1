94251e84513b473cff5ff716789da16f3a54219f
Gustavo <1999goncalo.gs@gmail.com>
2026-03-09 14:23:35 +0000

    exercicio 9

---
+++ exercicios_loops/exercicio9.ps1
@@ -0,0 +1,15 @@
+﻿do {
+    $num = Read-Host "Introduza um numero: "
+    $num = [int]$num
+    if ($num -ge 1 -and $num -le 100){
+        Write-Host "numero valido :)"
+        break
+    }
+    else {
+        write-host "Numero invalido :("
+      
+    }
+
+} while ($num -lt 1 -or $num -gt 100)
+ 
+
